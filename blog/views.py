854c6d11aebba98e98d6ef3ea1f81feb2cc97f66
vikas1188 <vikas.singh1188@gmail.com>
2016-07-16 21:16:58 +0530

    Removing the edit problem

--- blog/views.py
+++ blog/views.py
@@ -27,13 +27,13 @@ def post_new(request):
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
-		form1 = PostForm(request.POST, instance=post)
-		if form1.is_valid():
-			post = form1.save(commit=False)
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
 			post.author = request.user
 			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
-		form1 = PostForm(instance=post)
-	return render(request, 'blog/post_edit.html', {'form1': form1})
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
